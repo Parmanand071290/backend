1ad0ed79c63db35e7509be529453934c11d28434
Parmanand071290 <122513277+Parmanand071290@users.noreply.github.com>
2023-04-27 16:04:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,10 +16,4 @@ const result = add(3, 4);
 console.log(result);
 
 const db = require('./db');
-app.get('/products', (req, res) => {
 db.query('SELECT * FROM products WHERE id = ' + req.query.id);
-.then((product) => {
-...
-res.send(product);
-})
-});
